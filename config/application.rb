26bf0ea231bc3508789e805bbba747e22383279a
andoshin11 <shin.ando@meetus.jp>
2017-04-18 02:25:16 +0900

    Configure RSpec

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,9 @@ module ECSample
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.test_framework :rspec, view_specs: false, helper_specs: true, fixture: true, integration_tool: false
+      g.fixture_replacement :factory_girl, dir: 'spec/factories'
+    end
   end
 end
