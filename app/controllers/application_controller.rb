55a33f7ee148ff54d0fb307b5190e98d71ee9575
andoshin11 <shin.ando@meetus.jp>
2017-05-07 17:24:52 +0900

    Add current_cart method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+    def current_cart
+    # sessionはハッシュのようにアクセスできる
+    begin
+      Cart.find(session[:cart_id])
+    rescue ActiveRecord::RecordNotFound
+      # 新しいカートを作成する(DBも書き込む)
+      cart = Cart.create
+      session[:cart_id] = cart.id
+      cart
+    end
+  end
 end
