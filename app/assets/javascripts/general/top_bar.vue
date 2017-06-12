f97a6b183075771cbd4eb3e04673f64ee25c9c91
andoshin11 <shinglish11@gmail.com>
2017-06-12 09:18:27 +0900

    Update Header CSS

--- app/assets/javascripts/general/top_bar.vue
+++ app/assets/javascripts/general/top_bar.vue
@@ -1,27 +1,57 @@
 <template>
-
-  <div class="first-top-bar header-first">
-    <div class="container">
-      <div class="header-first__left">
-        <div class="call-us">
-          <a href="/" class="tell-number">
-            <i class="fa fa-phone"></i>
-            <span class="call-us__number">123 4567 8901</span>
+  <section class="p-top-bar">
+    <div class="header-first">
+      <div class="container">
+        <div class="header-first__left">
+          <div class="call-us">
+            <a href="/" class="tell-number">
+              <i class="fa fa-phone"></i>
+              <span class="call-us__number">123 4567 8901</span>
+            </a>
+          </div>
+        </div>
+        <div class="header-first__right">
+          <div class="header-first__right__wishlist">
+            <i class="fa fa-heart-o"></i>0
+          </div>
+          <a href="/cart" class="header-first__right__cart">
+            <i class="fa fa-shopping-bag"></i>
+            {{ cart.items.length }}
           </a>
         </div>
       </div>
-      <div class="header-first__right">
-        <div class="header-first__right__wishlist">
-          <i class="fa fa-heart-o"></i>0
-        </div>
-        <a href="/cart" class="header-first__right__cart">
-          <i class="fa fa-shopping-bag"></i>
-          {{ cart.items.length }}
-        </a>
+    </div>
+    <div class="header-second">
+      <div class="container">
+        <nav>
+          <a href="/" class="logo-container"></a>
+          <div class="nav-links-container">
+            <div class="dropdown">
+              <div class="nav-link">What's new</div>
+            </div>
+            <div class="dropdown">
+              <a href="/" class="nav-link dropdown-toggle active">Dresses</a>
+            </div>
+            <div class="dropdown">
+              <div class="nav-link">Designers</div>
+            </div>
+            <div class="dropdown">
+              <div class="nav-link">Accessories</div>
+            </div>
+            <div class="dropdown">
+              <div class="nav-link">Occasions</div>
+            </div>
+            <div class="dropdown">
+              <div class="nav-link">Fashion Pass</div>
+            </div>
+            <div class="dropdown">
+              <div class="nav-link">How it Works</div>
+            </div>
+          </div>
+        </nav>
       </div>
     </div>
-  </div>
-
+  </section>
 </template>
 
 <script>
